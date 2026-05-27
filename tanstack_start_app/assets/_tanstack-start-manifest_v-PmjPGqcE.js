46227f4e7320213b69c53225e8ab918d54a3afd0
kreatweb <kreatweb@users.noreply.github.com>
2026-05-27 08:07:07 +0000

    deploy: 4f4b61838c9ad5abd30ef3ab9d5df370e658b521

--- tanstack_start_app/assets/_tanstack-start-manifest_v-PmjPGqcE.js
+++
@@ -1,7 +0,0 @@
-const tsrStartManifest = () => ({
-  routes: {},
-  clientEntry: "/jungle-juniors-adventure/@id/virtual:tanstack-start-dev-client-entry"
-});
-export {
-  tsrStartManifest
-};
--- tanstack_start_app/index.js
+++
@@ -1,5 +0,0 @@
-import { w } from "./assets/worker-entry-B1VP3sr1.js";
-import "node:events";
-export {
-  w as default
-};
